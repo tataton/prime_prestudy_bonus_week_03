86683af30d6591d752268724c4dfdffa456ed015
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2016-10-25 15:45:02 -0500

    Created html, css and js files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# prime_prestudy_bonus_week_03
+Pre-Work Week 3 Prime Academy Bonus Assignment
---
+++ script.js
@@ -0,0 +1,13 @@
+function add(input) {
+  return (input + 2);
+}
+
+function multiply(input) {
+  return (input * 3);
+}
+
+function subtract(input) {
+  return (input - 4);
+}
+
+add(multiply(subtract(2)));
